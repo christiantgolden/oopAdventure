b76daa1daf4cfc004a54ada729ec4d9e82bc68d1
golden <christiantgolden@gmail.com>
2020-01-04 13:12:20 -0500

    modify javafx form to add canvas

--- src/com/company/UI/UserInterface.java
+++ src/com/company/UI/UserInterface.java
@@ -9,12 +9,16 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -51,7 +55,7 @@ public class UserInterface extends Application {
         final Text actiontarget = new Text();
         grid.add(actiontarget, 1, 6);
 
-        Scene scene = new Scene(grid, 300, 275);
+        Scene scene = new Scene(grid, 500, 500);
         scene.getStylesheets().add(UserInterface.class.getResource("UserInterface.css").toExternalForm());
         primaryStage.setScene(scene);
 
@@ -63,7 +67,15 @@ public class UserInterface extends Application {
         grid.add(userName, 0, 1);
 
         TextField userTextField = new TextField();
-        grid.add(userTextField, 1, 1);
+        grid.add(userTextField, 1, 1, 1, 1);
+
+        Canvas canvas = new Canvas(300, 250);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        drawShapes(gc);
+        grid.add(canvas, 1, 3);
+
+
+        //grid.setGridLinesVisible(true);
 
         ObservableList<String> options = FXCollections.observableArrayList(
                 "Wizard","Bard","Berserker","Dragonborn","Elf",
@@ -71,7 +83,8 @@ public class UserInterface extends Application {
         );
         final ComboBox comboBox = new ComboBox(options);
         comboBox.setValue("Race");
-        grid.add(comboBox,1, 2);
+        comboBox.setId("combobox");
+        grid.add(comboBox,2, 1);
 
         Button btn = new Button("Sign in");
         HBox hbBtn = new HBox(10);
@@ -84,10 +97,33 @@ public class UserInterface extends Application {
             public void handle(ActionEvent actionEvent) {
                 actiontarget.setId("actiontarget");
                 Character hero = new CharacterFactory().spawnCharacter(comboBox.getValue().toString(), userTextField.getText());
-                actiontarget.setText("You created: " + hero.getName() + ", a " + hero.getRace());
+                actiontarget.setText("You created: " + hero.getName() + ", a " + hero.getRace() + " at (" +
+                        hero.getX() + "," + hero.getY() + ")");
             }
         });
 
         primaryStage.show();
+    }
+    private void drawShapes(GraphicsContext gc){
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(5);
+        gc.strokeLine(40, 10, 10, 40);
+        gc.fillOval(10, 60, 30, 30);
+        gc.strokeOval(60, 60, 30, 30);
+        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
+        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
+        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
+        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
+        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
+        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
+        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
+        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
+        gc.fillPolygon(new double[]{10, 40, 10, 40},
+                new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolygon(new double[]{60, 90, 60, 90},
+                new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolyline(new double[]{110, 140, 110, 140},
+                new double[]{210, 210, 240, 240}, 4);
     }
 }
